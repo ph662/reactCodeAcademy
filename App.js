1b0e783d3b57142796b8d7fbe00c22cd1fc42da1
Phelipe Lima <phelipe662@gmail.com>
2022-01-20 10:56:22 -0300

    Changing the page.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> RAIFU </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'green',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
