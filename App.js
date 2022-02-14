1d799cc4f43b8ac15dc290ea2db9dad206edd883
Phelipe Lima <phelipe662@gmail.com>
2022-02-14 12:21:50 -0300

    Navigational structure - new tab for this screen

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-// const Stack = createStackNavigator();
-const Stack = createStackNavigator();
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const FeedScreen = () => (
-    <View style={styles.layout}>
-      <Text style={styles.title}>First screen</Text>
-    </View>
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+  </View>
+);
+
+const CatalogScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Catalog</Text>
+  </View>
+);
+
+const Stack = createBottomTabNavigator();
+
+export const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Feed" component={FeedScreen} />
+    <Stack.Screen name="Catalog" component={CatalogScreen} />
+  </Stack.Navigator>
 );
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Feed" component={FeedScreen} />
-    </Stack.Navigator>
+    <AppNavigator />
   </NavigationContainer>
 );
 
@@ -25,8 +35,8 @@ export default App;
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 32,
