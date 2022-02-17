c586fb1be28f98512a8193f9d7f93d38fb51213c
Phelipe Lima <phelipe662@gmail.com>
2022-02-17 17:14:33 -0300

    Conditional rendering

--- App.js
+++ App.js
@@ -1,34 +1,64 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { createContext, useContext, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const FeedScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>Feed</Text>
-  </View>
-);
+// Our global authentication state, with default values
+export const AuthContext = createContext({
+  hasUser: false,
+  setUser: () => {},
+});
 
-const CatalogScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>Catalog</Text>
-  </View>
-);
+const LoginScreen = () => {
+  const { setUser } = useContext(AuthContext);
 
-const Stack = createBottomTabNavigator();
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.title}>Login</Text>
+      <Button title="Login" onPress={() => setUser(true)}></Button>
+    </View>
+  );
+};
 
-export const AppNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Feed" component={FeedScreen} />
-    <Stack.Screen name="Catalog" component={CatalogScreen} />
-  </Stack.Navigator>
-);
+const FeedScreen = () => {
+  const { setUser } = useContext(AuthContext);
 
-const App = () => (
-  <NavigationContainer>
-    <AppNavigator />
-  </NavigationContainer>
-);
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.title}>Feed</Text>
+      <Button title="Login" onPress={() => setUser(false)}></Button>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+export const AppNavigator = () => {
+  const { hasUser } = useContext(AuthContext);
+  return (
+    <Stack.Navigator>
+      {hasUser ? (
+        <Stack.Screen name="Feed" component={FeedScreen} />
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  // This is linked to our global authentication state.
+  // We connect this in React to re-render components when changing this value.
+  const [hasUser, setUser] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ hasUser, setUser }}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
 
