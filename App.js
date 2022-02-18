65d34422f1e0d577c0999f8f4b69681cb85a10a7
Phelipe Lima <phelipe662@gmail.com>
2022-02-18 16:47:38 -0300

    button - navigate screen

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+const FeedScreen = () => {
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.title}>Feed</Text>
+      <Button
+       title="Go to catalog"
+        onPress={() => navigation.navigate('Catalog')}
+      />
+    </View>
+  );
+};
+
+const CatalogScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Catalog</Text>
+  </View>
+);
 
 const Stack = createStackNavigator();
 
-const OverviewScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>Overview</Text>
-  </View>
-);
-
-const ProfileNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Overview" component={OverviewScreen} />
-  </Stack.Navigator>
-);
-
-// Add the new stack navigator above this line
-
-const FeedScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>Feed</Text>
-  </View>
-);
-
-const Tab = createBottomTabNavigator();
-
 export const AppNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedScreen} />
-    <Tab.Screen name="Profile" component={ProfileNavigator} />
-  </Tab.Navigator>
+  <Stack.Navigator>
+    <Stack.Screen name="Feed" component={FeedScreen} />
+    <Stack.Screen name="Catalog" component={CatalogScreen} />
+  </Stack.Navigator>
 );
 
 const App = () => (
