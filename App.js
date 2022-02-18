55d9e8222d77446a5621f7b3dd15e92c9344b5e0
Phelipe Lima <phelipe662@gmail.com>
2022-02-18 16:38:34 -0300

    Tab navigation

--- App.js
+++ App.js
@@ -1,72 +1,53 @@
-import React, { createContext, useContext, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-// Our global authentication state, with default values
-export const AuthContext = createContext({
-  hasUser: false,
-  setUser: () => {},
-});
-
-const LoginScreen = () => {
-  const { setUser } = useContext(AuthContext);
-
-  return (
-    <View style={styles.layout}>
-      <Text style={styles.title}>Login</Text>
-      <Button title="Login" onPress={() => setUser(true)}></Button>
-    </View>
-  );
-};
-
-const FeedScreen = () => {
-  const { setUser } = useContext(AuthContext);
-
-  return (
-    <View style={styles.layout}>
-      <Text style={styles.title}>Feed</Text>
-      <Button title="Login" onPress={() => setUser(false)}></Button>
-    </View>
-  );
-};
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = () => {
-  const { hasUser } = useContext(AuthContext);
-  return (
-    <Stack.Navigator>
-      {hasUser ? (
-        <Stack.Screen name="Feed" component={FeedScreen} />
-      ) : (
-        <Stack.Screen name="Login" component={LoginScreen} />
-      )}
-    </Stack.Navigator>
-  );
-};
+const OverviewScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Overview</Text>
+  </View>
+);
 
-const App = () => {
-  // This is linked to our global authentication state.
-  // We connect this in React to re-render components when changing this value.
-  const [hasUser, setUser] = useState(false);
+const ProfileNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Overview" component={OverviewScreen} />
+  </Stack.Navigator>
+);
 
-  return (
-    <AuthContext.Provider value={{ hasUser, setUser }}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AuthContext.Provider>
-  );
-};
+// Add the new stack navigator above this line
+
+const FeedScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
+export const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={FeedScreen} />
+    <Tab.Screen name="Profile" component={ProfileNavigator} />
+  </Tab.Navigator>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+);
 
 export default App;
 
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 32,
