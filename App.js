56875f2f17c203c8cf16d884f96ccb69eb216b6e
Phelipe Lima <phelipe662@gmail.com>
2022-02-22 02:49:51 -0300

    Woofstagram-navigation

--- App.js
+++ App.js
@@ -1,41 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+import * as React from 'react';
+import { StyleSheet, Button, View, Text, TouchableOpacity } from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const FeedScreen = () => {
 
-  const navigation = useNavigation();
-
+//Nav Button Component
+const NavButton = ({screenName}) => {
+  const nav = useNavigation();
   return (
-    <View style={styles.layout}>
-      <Text style={styles.title}>Feed</Text>
-      <Button
-       title="Go to catalog"
-        onPress={() => navigation.navigate('Catalog')}
-      />
-    </View>
-  );
-};
+    <TouchableOpacity onPress={() => {nav.navigate(screenName)}} style={styles.button} >
+        <Text style={styles.buttonText}>{screenName}</Text>
+    </TouchableOpacity>
+  )
+}
 
-const CatalogScreen = () => (
+// --- Main screens ---
+const HomeScreen = () => (
   <View style={styles.layout}>
-    <Text style={styles.title}>Catalog</Text>
+    <Text style={styles.title}>Home</Text>
   </View>
 );
 
-const Stack = createStackNavigator();
+const FeedScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+  </View>
+);
 
-export const AppNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Feed" component={FeedScreen} />
-    <Stack.Screen name="Catalog" component={CatalogScreen} />
+const OverviewScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Overview</Text>
+  </View>
+);
+
+const AccountScreen = () => (
+  <View style={styles.layout}>
+    <SignInScreen/>
+  </View>
+);
+
+// --- Login Screens ---
+const SignInScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Sign In</Text>
+    <NavButton screenName="Sign Up"/>
+  </View>
+);
+
+const SignUpScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Sign Up</Text>
+    <NavButton screenName="Main"/>
+  </View>
+);
+
+// --- App ---
+
+//Tab Navigation
+const Tab = createBottomTabNavigator();
+export const MainNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Overview" component={OverviewScreen} />
+    <Tab.Screen name="Feed" component={FeedScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+//Stack Navigation
+const Stack = createStackNavigator();
+export const ProfileNavigator = () => (
+  <Stack.Navigator  >
+    <Stack.Screen name="Sign In" component={SignInScreen} options={{headerShown: false}} />
+    <Stack.Screen name="Sign Up" component={SignUpScreen} options={{headerShown: false}} />
+    <Stack.Screen name="Main" component={MainNavigator} options={{headerShown: false}}/>
   </Stack.Navigator>
 );
 
+
 const App = () => (
-  <NavigationContainer>
-    <AppNavigator />
+   <NavigationContainer>
+    <ProfileNavigator />
   </NavigationContainer>
 );
 
@@ -45,10 +92,21 @@ const styles = StyleSheet.create({
   layout: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    padding: 8,
   },
   title: {
-    fontSize: 32,
-    marginBottom: 16,
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
+  button: {
+    padding: 15,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18
+  }
 });
